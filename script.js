fc913dea834806ee9190e497f2808be78a8014f9
AnneNjue <keenlea393@gmail.com>
2026-05-30 11:07:52 +0300

    updated script.js

--- script.js
+++ script.js
@@ -135,8 +135,7 @@ const contactForm = document.getElementById('contactForm');
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  // Here you would typically send the form data to a server
-  // For now, we'll just show an alert
+  
   alert('Thank you for your message! I will get back to you soon.');
   contactForm.reset();
 });
